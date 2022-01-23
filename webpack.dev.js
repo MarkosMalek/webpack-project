551c6b8b4e191eb61cb42da101065ae4548b1882
MarkousMalek <MarkousMalek@gmail.com>
2022-01-23 20:32:58 +0200

    adding service worker

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
 const htmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
   mode: "development",
   devtool: "source-map",
